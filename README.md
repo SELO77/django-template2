8a90a1f7fdaab162cc80b326e20041190860be1c
seloselo1001 <seloselo1001@gmail.com>
2019-04-26 21:22:53 +0900

    Write mysql and redis running command to README.md

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 # bbakdoc
 
 ```bash
+docker run --detach --env MYSQL_USER=root --env MYSQL_ALLOW_EMPTY_PASSWORD=yes \
+ --rm \
+ --volume $PWD/.mysql:/var/lib/mysql  -p 3306:3306 \
+ --name=mysql-5.6.41 \
+ mysql:5.6.41
+```
 
+```bash
+docker run -p 6379:6379 -d redis:2.8
 ```
 
 
--- bbakdoc/events/consumers.py
+++ bbakdoc/events/consumers.py
@@ -1,10 +1,16 @@
 import json
 
-from asgiref.sync import async_to_sync
+from channels.db import database_sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
 
 
+# @database_sync_to_async
+# def get_user_email(scope):
+#     return scope.user.email
+
+
 class EventConsumer(AsyncWebsocketConsumer):
+
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = f'event_{self.room_name}'
@@ -27,33 +33,28 @@ class EventConsumer(AsyncWebsocketConsumer):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
 
+        email = self.scope['user'].email
+
         # Send message to room group
         await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'chat_message',
                 'message': message,
-                # 'user': self.scope.user,
-                # 'email': self.scope.user.email,
+                'email': email,
             }
         )
-        # self.send(
-        #     text_data=json.dumps({
-        #         'message': message,
-        #         # 'user': self.scope.user,
-        #     })
-        # )
 
     # Receive message from room group
     async def chat_message(self, event):
         message = event['message']
-        # email = event['email']
+        email = event.get('email', self.channel_name)
         # user = event['user']
 
         await self.send(
             text_data=json.dumps({
                 'message': message,
                 # 'user': user,
-                # 'email': email,
+                'email': email,
             })
         )

715374b87e766a76a1f9fa85bc1477751d838dff
zinee <zineeworld@gmail.com>
2019-05-26 19:56:26 +0900

    <markup>: main page

--- bbakdoc/events/urls.py
+++ bbakdoc/events/urls.py
@@ -7,7 +7,7 @@ app_name = 'events'
 
 
 urlpatterns = [
-    path('', view=EventMainView.as_view(), name='main'),
+    path('', view=EventMainView.as_view(), name='page'),
     path('create/', view=EventCreateView.as_view(), name='event_create'),
     path('<int:pk>/', view=EventDetailView.as_view(), name='event_detail'),
     path('<int:pk>/activate/', view=EventToggleActivateView.as_view(), name='event_detail'),
